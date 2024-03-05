5bceab5abd8efb7f643880f38f9c7194dae43871
ivDivan <150170769+ivDivan@users.noreply.github.com>
2024-03-05 11:03:08 +0100

    Add files via upload

---
+++ Class02Homework/AverageNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter First Number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Second Number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Third Number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Fourth Number: ");
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+int avg = (num1 + num2 + num3 + num4) / 4;
+
+Console.WriteLine($"The average of {num1}, {num2}, {num3} and {num4} is: {avg}");
---
+++ Class02Homework/SwapNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input First Number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Second Number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int tempNum = num1;
+num1 = num2;
+num2 = tempNum;
+
+Console.WriteLine("After Swapping: ");
+Console.WriteLine($"First Number: {num1}");
+Console.WriteLine($"Second Number: {num2}");
