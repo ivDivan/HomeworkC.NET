4b11d888d9f655f69bbff801aaefbaaf9ef0e243
ivDivan <150170769+ivDivan@users.noreply.github.com>
2024-03-09 15:39:46 +0100

    Add files via upload

---
+++ Class03Homework/Class03Homework.SumOfEven/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] sumArray = new int[6];
+int sum = 0;
+
+for (int i = 0; i < sumArray.Length; i++)
+{
+    Console.WriteLine($"Enter integer no.{i + 1}");
+    if (int.TryParse(Console.ReadLine(), out sumArray[i]))
+    {
+        if (sumArray[i] % 2 == 0)
+        {
+            sum += sumArray[i];
+        }
+    }
+    else
+    {
+        Console.WriteLine("You got yourself an oooopsiiieee!!! Mebbe try again?");
+        i--;
+    }
+}
+
+Console.WriteLine($"The result is: {sum}");
