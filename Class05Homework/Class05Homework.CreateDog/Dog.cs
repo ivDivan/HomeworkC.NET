92282c87698375f4c9a58f4e2961966c21aabe7e
ivDivan <150170769+ivDivan@users.noreply.github.com>
2024-03-12 10:51:49 +0100

    Add files via upload

---
+++ Class05Homework/Class05Homework.CreateDog/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class05Homework.CreateDog
+{
+    public class Dog
+    {
+        public string Name { get; set; }   
+        public string Breed { get; set; }
+        public string Color {  get; set; }
+        public string Eat()
+        {
+            return $"{Name}, the {Color} {Breed}, is now eating.";
+        }
+        public string Play()
+        {
+            return $"{Name}, the {Color} {Breed}, is now playing.";
+        }
+        public string ChaseTail()
+        {
+            return $"{Name}, the {Color} {Breed}, is now chasing its tail.";
+        }
+    }
+}
+
